311c631889243df9756daab07ca5717c7387397b
Yale-G <tasosg77@gmail.com>
2019-12-10 00:57:32 +0200

    allages sto db final

--- app/src/main/java/com/example/weatherforecastapp/MainActivity.java
+++ app/src/main/java/com/example/weatherforecastapp/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     public void viewAllHistory() {
         Button1.setOnClickListener(
                 new View.OnClickListener() {
