04b80fcecd40ca076d222c5c0f13db90a9a50658
pascal80 <48256234+pascal80@users.noreply.github.com>
2019-12-17 15:38:19 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/weatherforecastapp/MainActivity.java
+++ app/src/main/java/com/example/weatherforecastapp/MainActivity.java
@@ -260,4 +260,38 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
+    private EditText editTxt;
+    private Button btn;
+    private ListView list;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> arrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTxt = (EditText) findViewById(R.id.editText);
+        btn = (Button) findViewById(R.id.button);
+        list = (ListView) findViewById(R.id.listView);
+        arrayList = new ArrayList<String>();
+
+        // Adapter: You need three parameters 'the context, id of the layout (it will be where the data is shown),
+        // and the array that contains the data
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, arrayList);
+
+        // Here, you set the data in your ListView
+        list.setAdapter(adapter);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // this line adds the data of your EditText and puts in your array
+                arrayList.add(editTxt.getText().toString());
+                // next thing you have to do is check if your adapter has changed
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
     }
